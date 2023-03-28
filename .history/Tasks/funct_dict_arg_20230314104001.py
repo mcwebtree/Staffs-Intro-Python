8a2e5e65cc7626eea8e40ad83425c749def852e2
Mark <github@mcwebtree.com>
2023-03-28 12:27:13 +0100

    Delete .history/Tasks directory

--- .history/Tasks/funct_dict_arg_20230314104001.py
+++
@@ -1,7 +0,0 @@
-
-def ex3 ( **args ):
-    for key, value in args.items():
-        print ( key + ": " + value)
-
-
-ex3 (bob="1", dave="2", steve="3")
--- .history/Tasks/funct_dict_arg_20230314104223.py
+++
@@ -1,7 +0,0 @@
-
-def ex3 ( **kwargs  ):
-    for key, value in kwargs.items():
-        print ( key + ": " + value)
-
-
-ex3 (bob="1", dave="2", steve="3")
